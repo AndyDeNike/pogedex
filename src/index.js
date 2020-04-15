81c52039b540f7cb766b03383a910e799d0a6186
Andydenike <denikeandrew@gmail.com>
2020-04-14 22:00:40 -0700

    Added font

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import './PKMNRBYGSC.ttf'
 
 import { ApolloClient } from "apollo-client";
 import { InMemoryCache } from "apollo-cache-inmemory";
